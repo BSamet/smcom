113c0c30d953fe27b25a80dc32949434fda2c77c
Florian Gi <86287152+Saphii@users.noreply.github.com>
2021-11-17 09:28:14 +0100

    Menu Navbar déroulant
    création du menu et bouton déroulant de la barre de navigation supérieur.

--- src/app/components/dashboardpage/navbar/navbar.component.ts
+++ src/app/components/dashboardpage/navbar/navbar.component.ts
@@ -9,9 +9,16 @@ import {TokenStorageService} from "../../../services/token-storage.service";
 export class NavbarComponent implements OnInit {
   user:any;
   constructor(private tokenStorage: TokenStorageService) { }
-
+  menus = [false, false]
+  isOpenedList = this.menus;
+  toggle(index:number) {
+    this.isOpenedList[index] = !this.isOpenedList[index];
+  }
+  closeMenu() {
+    this.isOpenedList = this.menus;
+  }
   ngOnInit(): void {
-    this.user = this.tokenStorage.getUser();
+    this.user = this.tokenStorage.getUser()
   }
 
 }
