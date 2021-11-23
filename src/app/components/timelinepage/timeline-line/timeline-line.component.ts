6ec782caa96d09bbf65f866c743fae4b0ff97acd
Samet BULUT <samet.bulut@uha.fr>
2021-11-23 15:18:40 +0100

    Samet  : Timeline => Display data from api

--- src/app/components/timelinepage/timeline-line/timeline-line.component.ts
+++ src/app/components/timelinepage/timeline-line/timeline-line.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {TimelineService} from "../../../services/timeline.service";
+import * as moment from 'moment';
 
 // Start ApexCharts Import
 import {
@@ -8,6 +10,7 @@ import {
   ApexXAxis,
   ApexTitleSubtitle
 } from "ng-apexcharts";
+import {TimelineData} from "../../../interfaces/timeline";
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -24,438 +27,94 @@ export type ChartOptions = {
 })
 export class TimelineLineComponent implements OnInit {
 
+  dataTimeline!: TimelineData[];
+  series = [] as any;
+  data: any[];
+
   @ViewChild("chart") chart: ChartComponent | undefined;
   public chartOptions: Partial<ChartOptions> | any;
 
-
-  constructor() {
+  constructor(private timelineService: TimelineService) {
     // Timeline Chart Data
-    this.chartOptions = {
-      series: [
-        {
-          name: "Production",
-          data: [
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 08:00:00").getTime(),
-                new Date("2019-03-05 11:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 15:00:00").getTime(),
-                new Date("2019-03-05 17:00:00").getTime()
-              ]
-            },
-          ],
-          color: "#329932"
-        },
-        {
-          name: "Failure",
-          data: [
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 11:00:00").getTime(),
-                new Date("2019-03-05 13:00:00").getTime()
-              ]
-            },
-          ],
-          color: "#FF1919"
-        },
-        {
-          name: "Setup",
-          data: [
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 14:00:00").getTime(),
-                new Date("2019-03-05 15:00:00").getTime()
-              ]
-            },
-          ],
-          color: "#3b82f6"
-        },
-        {
-          name: "Idle",
-          data: [
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 13:00:00").getTime(),
-                new Date("2019-03-05 14:00:00").getTime()
-              ]
-            },
-          ],
-          color: "#FFAE19"
-        },
-        {
-          name: "Disengagement",
-          data: [
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Lundi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mardi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Mercredi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Jeudi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Vendredi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Samedi",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05").getTime(),
-                new Date("2019-03-05 08:00:00").getTime()
-              ]
-            },
-            {
-              x: "Dimanche",
-              y: [
-                new Date("2019-03-05 17:00:00").getTime(),
-                new Date("2019-03-06").getTime()
-              ]
-            },
-          ],
-          color: "#BFBFBF"
-        },
-      ],
-      chart: {
-        height: 650,
-        type: "rangeBar",
-      },
-      plotOptions: {
-        bar: {
-          horizontal: true,
-          barHeight: "50%",
-          rangeBarGroupRows: true,
-        },
-      },
-      xaxis: {
-        type: "datetime",
-        labels: {
-          style: {
-            fontSize: '17px'
-          }
-        }
-      },
-      yaxis: {
-        labels: {
-          style: {
-            fontSize: '17px'
-          }
-        }
-      },
-      legend: {
-        position: "top",
-        horizontalAlign: "center",
-        fontSize: "17px",
-        onItemClick: {
-          toggleDataSeries: true
-        },
-      },
-      tooltip: {
-        enabled: true,
-        x: {
-          format: 'dd MMMM HH:mm',
-        },
-      },
-      title: {
-        text: 'Semaine 1, Du 01/02/2021 au 07/02/2021',
-        style: {
-          fontSize: '20px'
-        }
-      }
-    };
+    this.series = [];
+    this.data = [];
   }
 
   ngOnInit(): void {
+    this.timelineService.timelineData()
+      .subscribe(data => {
+        this.dataTimeline = data as TimelineData[]
+        console.log(this.dataTimeline);
+        for (let top of this.dataTimeline) {
+          const start = moment(top.topstartdatefield, 'DD-MM-YYYY HH-mm-ss').format('YYYY-MM-DD HH-mm-ss')
+          const end = moment(top.topenddatefield, 'DD-MM-YYYY HH-mm-ss').format('YYYY-MM-DD HH-mm-ss')
+          this.data.push(
+            [
+              {
+                x: top.topstatehandlefield,
+                y: [
+                  start,
+                  end
+                ]
+              }
+            ]
+          )
+        }
+
+        this.series.push({name: "Production", data: this.data, color: "#329932"})
+        console.log(this.series)
+        this.chartOptions = {
+          series:
+          this.series,
+          chart: {
+            height: 650,
+            type: "rangeBar",
+          },
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              barHeight: "50%",
+              rangeBarGroupRows: true,
+            },
+          },
+          xaxis: {
+            type: "datetime",
+            labels: {
+              style: {
+                fontSize: '17px'
+              }
+            }
+          },
+          yaxis: {
+            labels: {
+              style: {
+                fontSize: '17px'
+              }
+            }
+          },
+          legend: {
+            position: "top",
+            horizontalAlign: "center",
+            fontSize: "17px",
+            onItemClick: {
+              toggleDataSeries: true
+            },
+          },
+          tooltip: {
+            enabled: true,
+            x: {
+              format: 'dd MMMM HH:mm',
+            },
+          },
+          title: {
+            text: 'Semaine 1, Du 01/02/2021 au 07/02/2021',
+            style: {
+              fontSize: '20px'
+            }
+          }
+        };
+      });
   }
+
 
 }
