e0fa4a909342e0bc55b2545471c6b31324ac2f1c
damien.allgaier <damien.allgaier@uha.fr>
2021-12-06 10:17:36 +0100

    responsive de la dashboard fonctionnel jusqu'à 360px

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    screen:{
-      "xs":"420px",
+    screens: {
+      'xs':'360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     colors:{
       primary_cwork: "#324B5D",
