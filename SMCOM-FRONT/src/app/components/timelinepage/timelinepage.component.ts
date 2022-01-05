e546b630985f5f357f3fdf17a3f95068bae9f8aa
Vince <contact@vmorales.fr>
2022-01-05 14:16:13 +0100

    feat(timeline) : slide buttons and ngModels + dynamic change (SMCOM#143)

--- SMCOM-FRONT/src/app/components/timelinepage/timelinepage.component.ts
+++ SMCOM-FRONT/src/app/components/timelinepage/timelinepage.component.ts
@@ -3,6 +3,7 @@ import {animCloseOpen, flyInOut} from "../../animations/animations";
 import { LanguageService } from 'src/app/services/language.service';
 import {TimelineService} from "../../services/timeline.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import {MatDatepickerInputEvent} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-timelinepage',
@@ -26,25 +27,19 @@ export class TimelinepageComponent implements OnInit {
   constructor(private language:LanguageService, private timelineService: TimelineService) { }
 
   ngOnInit(): void {
-    const maxDay = new Date(2021, 10, 30);
-    const sixDaysPrior = maxDay.getTime() - 6*86400000;
-    this.daysList = this.timelineService.getDaysArray(new Date(sixDaysPrior), maxDay);
+    // const maxDay = new Date(2021, 10, 30);
+    // const sixDaysPrior = maxDay.getTime() - 6*86400000;
+    // this.daysList = this.timelineService.getDaysArray(new Date(sixDaysPrior), maxDay);
     this.isShowKpi = false;
     this.isShowTimeline = true;
     this.isSideNavPin = false;
   }
 
-  getTextFromKey(key:string){
-    return this.language.getTextFromKey(key)
-  }
-  toggleShowKpi() {
-    this.isShowKpi = ! this.isShowKpi;
-  }
-  toggleShowTimeline() {
-    this.isShowTimeline = ! this.isShowTimeline;
-  }
-
   toggleSideNavPin() {
     this.isSideNavPin = ! this.isSideNavPin;
+  }
+
+  updateTimelines(change: string, $event: MatDatepickerInputEvent<Date>) {
+    this.daysList = this.timelineService.getDaysArray(new Date(this.range.value.start), new Date(this.range.value.end));
   }
 }
