f89cb6888643c5ce3357cce31bdd308dbb765738
hugo.staedelin <hugo.staedelin@uha.fr>
2022-01-10 13:50:06 +0100

    feat(script): edit script file, adding execution process for starting nestjs, angular and json server node runtime, and working directory path.
    To work, the script requires the absolute path to the smcom working directory.

--- setup.ps1
+++ setup.ps1
@@ -1,53 +1,70 @@
 [void][Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+
+# Initialize GUI components
 $smcomInit = New-Object System.Windows.Forms.Form
-$smcomInit.Text = 'SMCOM Script Setup'
-
 $startBtn = New-Object System.Windows.Forms.Button
-$stopBtn = New-Object System.Windows.Forms.Button
 $quitBtn = New-Object System.Windows.Forms.Button
+$exploreBtn = New-Object System.Windows.Forms.Button
+$projectDirectoryPathLabel = New-Object System.Windows.Forms.Label
+$scriptInitialized = New-Object System.Windows.Forms.Label
+$projectDirectoryPath = New-Object System.Windows.Forms.TextBox
+$folderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
 
-$nestMonitor = New-Object System.Windows.Forms.Label
-$angularMonitor = New-Object System.Windows.Forms.Label
-$jsonServerMonitor = New-Object System.Windows.Forms.Label
+# ProjectDirectoryPathLabel config
+$projectDirectoryPathLabel.AutoSize = $true
+$projectDirectoryPathLabel.Text = "Please specify the SMCOM root folder path:"
+$projectDirectoryPathLabel.Location = New-Object System.Drawing.Point(20,10)
 
-$nestMonitor.AutoSize = $true
-$nestMonitor.Location = New-Object System.Drawing.Point(30,20)
-$nestMonitor.Text = 'NestJS status: offline'
+# ProjectDirectoryPath config
+$projectDirectoryPath.Location = New-Object System.Drawing.Point(20,30)
+$projectDirectoryPath.Size = New-Object System.Drawing.Size(210, 30)
 
-$angularMonitor.AutoSize = $true
-$angularMonitor.Location = New-Object System.Drawing.Point(30,40)
-$angularMonitor.Text = 'Angular status: offline'
+# ExplorerBtn config
+$exploreBtn.Text = "Browse..."
+$exploreBtn.Location = New-Object System.Drawing.Point(250,28)
+$exploreBtn.AutoSize = $true
+$exploreBtn.Add_Click({
+    $show = $folderBrowserDialog.ShowDialog()
+    If($Show -eq "OK"){
+        $projectDirectoryPath.Text = $folderBrowserDialog.SelectedPath
+        $rootProjectPath = $projectDirectoryPath.Text
+    }
+})
 
-$jsonServerMonitor.AutoSize = $true
-$jsonServerMonitor.Location = New-Object System.Drawing.Point(30,60)
-$jsonServerMonitor.Text = 'JSON server status: offline'
+#scriptInitialized config
+$scriptInitialized.AutoSize = $true
+$scriptInitialized.Location = New-Object System.Drawing.Point(25,70)
 
+#startBtn config
 $startBtn.AutoSize = $true
 $startBtn.Location = New-Object System.Drawing.Point(30,100)
 $startBtn.Text = 'Start SMCOM'
 $startBtn.Add_Click({
+    cd $rootProjectPath
+    cd '.\SMCOM-BACK'
+    Start-Process npm -ArgumentList "install" -PassThru
+    Start-Process npm -ArgumentList "run start" -PassThru
+    cd '..\SMCOM-FRONT'
+    Start-Process npm -ArgumentList "install" -PassThru
+    Start-Process npm -ArgumentList "run start" -PassThru
+    Start-Process npm -ArgumentList "run mock:server" -PassThru
 
+    $scriptInitialized.Text = "Process are running ..."
 })
 
-$stopBtn.AutoSize = $true
-$stopBtn.Location = New-Object System.Drawing.Point(120,100)
-$stopBtn.Text = 'Stop SMCOM'
-$stopBtn.Add_Click({
-
-})
-
+#quitBtn config
 $quitBtn.AutoSize = $true
-$quitBtn.Location = New-Object System.Drawing.Point(210,100)
+$quitBtn.Location = New-Object System.Drawing.Point(250,100)
 $quitBtn.Text = 'Exit'
-
 $quitBtn.Add_Click({$smcomInit.Close()})
 
-$smcomInit.Size = New-Object System.Drawing.Size(325, 200)
-$smcomInit.Controls.Add($nestMonitor)
-$smcomInit.Controls.Add($angularMonitor)
-$smcomInit.Controls.Add($jsonServerMonitor)
+#SMCOMInit config
+$smcomInit.Text = 'SMCOM Development Script Setup'
+$smcomInit.Size = New-Object System.Drawing.Size(350, 200)
+$smcomInit.Controls.Add($exploreBtn)
+$smcomInit.Controls.Add($projectDirectoryPath)
+$smcomInit.Controls.Add($projectDirectoryPathLabel)
+$smcomInit.Controls.Add($scriptInitialized)
 $smcomInit.Controls.Add($startBtn)
-$smcomInit.Controls.Add($stopBtn)
 $smcomInit.Controls.Add($quitBtn)
-
 $smcomInit.ShowDialog()
