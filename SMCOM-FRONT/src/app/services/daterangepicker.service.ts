c031a288f6b4b52b0d1c864d23afa6b96d66502f
hugo.staedelin <hugo.staedelin@uha.fr>
2022-01-25 17:20:13 +0100

    fix: add date range interval for all options

--- SMCOM-FRONT/src/app/services/daterangepicker.service.ts
+++ SMCOM-FRONT/src/app/services/daterangepicker.service.ts
@@ -1,67 +1,70 @@
 import { Injectable } from '@angular/core';
-import {DateRange, MatDateRangeSelectionStrategy} from "@angular/material/datepicker";
+import {DateRange} from "@angular/material/datepicker";
 import {DateAdapter} from "@angular/material/core";
+import {TimelineService} from "./timeline.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DaterangepickerService<D> implements MatDateRangeSelectionStrategy<D> {
+export class DaterangepickerService<D> {
   private range?: string;
-  constructor(private dateAdapter: DateAdapter<D>) {}
+  private start!: Date;
+  private end!: Date;
+  constructor(private dateAdapter: DateAdapter<Date>, private timelineService: TimelineService) {}
 
-  selectionFinished(date: D): DateRange<D> {
+  selectionFinished(date: Date): DateRange<Date> {
     return this.checkRange(date);
   }
 
-  createPreview(activeDate: D): DateRange<D> {
+  createPreview(activeDate: Date): DateRange<Date> {
     return this.checkRange(activeDate);
   }
 
-  private createDayRange(date: D | null): DateRange<D> {
+  private createDayRange(date: Date | null): DateRange<Date> {
     if (date) {
-      const start = this.dateAdapter.addCalendarDays(date, 0);
-      const end = this.dateAdapter.addCalendarDays(date, 0);
-      return new DateRange<D>(start, end);
+      this.start = this.dateAdapter.addCalendarDays(date, 0);
+      this.end = this.dateAdapter.addCalendarDays(date, 0);
+      return new DateRange<Date>(this.start, this.end);
     }
 
-    return new DateRange<D>(null, null);
+    return new DateRange<Date>(null, null);
   }
 
-  private createWeekRange(date: D | null): DateRange<D> {
+  private createWeekRange(date: Date | null): DateRange<Date> {
     if (date) {
-      const start = this.dateAdapter.addCalendarDays(date, 0);
-      const end = this.dateAdapter.addCalendarDays(date, 6);
-      return new DateRange<D>(start, end);
+      this.start = this.dateAdapter.addCalendarDays(date, 0);
+      this.end = this.dateAdapter.addCalendarDays(date, 6);
+      return new DateRange<Date>(this.start, this.end);
     }
 
-    return new DateRange<D>(null, null);
+    return new DateRange<Date>(null, null);
   }
 
-  private createMonthRange(date: D | null): DateRange<D> {
+  private createMonthRange(date: Date | null): DateRange<Date> {
     if (date) {
-      const start = this.dateAdapter.addCalendarDays(date, 0);
-      const end = this.dateAdapter.addCalendarDays(date, 30);
-      return new DateRange<D>(start, end);
+      this.start = this.dateAdapter.addCalendarDays(date, 0);
+      this.end = this.dateAdapter.addCalendarDays(date, this.dateAdapter.getNumDaysInMonth(date)-1);
+      return new DateRange<Date>(this.start, this.end);
     }
 
-    return new DateRange<D>(null, null);
+    return new DateRange<Date>(null, null);
   }
 
-  private createYearRange(date: D | null): DateRange<D> {
+  private createYearRange(date: Date | null): DateRange<Date> {
     if (date) {
-      const start = this.dateAdapter.addCalendarDays(date, 0);
-      const end = this.dateAdapter.addCalendarDays(date, 365);
-      return new DateRange<D>(start, end);
+      this.start = this.dateAdapter.addCalendarDays(date, 0);
+      this.end = this.dateAdapter.addCalendarYears(date, 1);
+      return new DateRange<Date>(this.start, this.end);
     }
 
-    return new DateRange<D>(null, null);
+    return new DateRange<Date>(null, null);
   }
 
   changeRange(dateItem: string) {
     this.range = dateItem;
   }
 
-  checkRange(date: D){
+  checkRange(date: Date){
     switch (this.range) {
       case "Jour":
         return this.createDayRange(date);
@@ -74,5 +77,9 @@ export class DaterangepickerService<D> implements MatDateRangeSelectionStrategy<
       default:
         return this.createDayRange(date);
     }
+  }
+
+  updateDayList(){
+    return this.timelineService.getDaysArray(this.start, this.end);
   }
 }
