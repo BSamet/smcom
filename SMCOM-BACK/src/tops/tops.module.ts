3b3bb75f3b48631152fcca52899c78e5af8aca32
Vincent <empty>
2022-02-03 16:59:03 +0100

    fix: edited build configuration (SMCOM#247bis)

--- SMCOM-BACK/src/tops/tops.module.ts
+++ SMCOM-BACK/src/tops/tops.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TopsService } from './tops.service';
 import { TopsController } from './tops.controller';
+import {AuthModule} from "../auth/auth.module";
+import {HttpModule} from "@nestjs/axios";
 
 @Module({
+  imports: [AuthModule, HttpModule],
   controllers: [TopsController],
   providers: [TopsService]
 })
