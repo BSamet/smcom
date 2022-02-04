c59134d43415853f23ae33c6b286fa2425e6b1ab
hugo.staedelin <hugo.staedelin@uha.fr>
2022-02-04 15:31:52 +0100

    refactor: add two date selectors with their respective behavior.
    One of the components corresponds to the old implementation of ```MatDateRangeSelectionStrategy``` that was associated with the timeline component, and the other component corresponds to the custom date selector.
    
    To work, the ```PresetDateSelectorComponent```, has retrieved the refactored methods of ```TimelinePageComponent```, and recalls the ```TimelineService``` and ```DateRangeService``` classes.
    
    For the ```CustomDateSelectorComponent```, it is a basic implementation of the DatePicker component with the necessary options.
    
    The use of the ```Input()``` allows to give the date selectors the range and the list of dates, and the ```Output``` are in charge of returning this updated list to the timeline component for display.

--- SMCOM-FRONT/src/app/app.module.ts
+++ SMCOM-FRONT/src/app/app.module.ts
@@ -32,10 +32,12 @@ import {MatNativeDateModule} from "@angular/material/core";
 import { ServicecheckerComponent } from './components/servicechecker/servicechecker.component';
 import { AbilityModule } from '@casl/angular';
 import { Ability, PureAbility } from '@casl/ability';
-import {NgxDaterangepickerMd} from "ngx-daterangepicker-material";
 import { PermissionsComponent } from './components/permissions/permissions.component';
 import {MatListModule} from "@angular/material/list";
 import {MatSelectModule} from "@angular/material/select";
+import {PresetDateSelectorComponent} from "./components/preset-date-selector/preset-date-selector.component";
+import { CustomDateSelectorComponent } from './components/custom-date-selector/custom-date-selector.component';
+import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
@@ -58,7 +60,9 @@ import {MatIconModule} from "@angular/material/icon";
     TimelinePeriodComponent,
     TimelinenavigationComponent,
     ServicecheckerComponent,
-    PermissionsComponent
+    PermissionsComponent,
+    PresetDateSelectorComponent,
+    CustomDateSelectorComponent
   ],
   imports: [
     BrowserModule,
@@ -79,7 +83,9 @@ import {MatIconModule} from "@angular/material/icon";
     MatDatepickerModule,
     MatListModule,
     MatSelectModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [
     { provide: Ability, useValue: new Ability() },
---
+++ SMCOM-FRONT/src/app/components/custom-date-selector/custom-date-selector.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {TimelineService} from "../../services/timeline.service";
+
+@Component({
+  selector: 'custom-date-selector',
+  templateUrl: './custom-date-selector.component.html',
+  styleUrls: ['./custom-date-selector.component.css']
+})
+export class CustomDateSelectorComponent implements OnInit {
+  @Input() dayList!: Date[];
+  @Output() timelineUpdated = new EventEmitter();
+
+  customRange = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  constructor(private timelineService: TimelineService) { }
+
+  ngOnInit(): void {
+  }
+
+  timelineUpdate(){
+    this.updateTimelines();
+    this.timelineUpdated.emit(this.dayList);
+  }
+
+  updateTimelines() {
+    this.dayList = this.timelineService.getDaysArray(this.customRange.value.start, this.customRange.value.end);
+  }
+}
